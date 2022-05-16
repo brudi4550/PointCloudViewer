f145dccde2147e49c424fff0b9b2303b7f14f257
Alexander Wolf <alexanderwolf@outlook.at>
2022-05-16 10:45:10 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'test',
+  })
+})
+
+app.listen(port, () => {
+  console.log(`PointCloudViewer listening on port ${port}`)
+})
