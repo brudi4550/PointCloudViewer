e1f6b25824bc60ac3ecc8ca55a9b2bdb9695fdee
Alexander Wolf <alexanderwolf@outlook.at>
2022-05-30 16:53:16 +0200

    added file upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const busboy = require('connect-busboy');   // Middleware to handle the file upload https://github.com/mscdex/connect-busboy
+const path = require('path');               // Used for manipulation with path
+const fs = require('fs-extra');             // Classic fs
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -7,11 +10,42 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+app.use(busboy({
+    highWaterMark: 2 * 1024 * 1024, // Set 2MiB buffer
+})); // Insert the busboy middle-ware
+
+const uploadPath = path.join(__dirname, 'fu/'); // Register the upload path
+fs.ensureDir(uploadPath); // Make sure that he upload path exits
+
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'PointCloudViewer',
   })
 })
+
+app.get('/upload', (req, res) => {
+  res.render('upload', {
+    title: 'PointCloudViewer',
+  })
+})
+
+app.route('/upload').post((req, res, next) => {
+    console.log('received post request');
+    req.pipe(req.busboy); // Pipe it trough busboy
+    req.busboy.on('file', (fieldname, file, filename) => {
+        console.log(`Upload of '${filename}' started`);
+        // Create a write stream of the new file
+        //fix naming + file type of uploaded file
+        const fstream = fs.createWriteStream(path.join(uploadPath, fieldname));
+        // Pipe it trough
+        file.pipe(fstream);
+        // On finish of the upload
+        fstream.on('close', () => {
+            console.log(`Upload of '${filename}' finished`);
+            res.redirect('back');
+        });
+    });
+});
 
 app.listen(port, () => {
   console.log(`PointCloudViewer listening on port ${port}`)
--- public/js/main.js
+++ public/js/main.js
@@ -21,14 +21,3 @@ navbar_brand.addEventListener('click', () => {
     var loc = window.location;
     window.location = loc.protocol + '//' + loc.host + loc.pathname + loc.search;
 });
-
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-    tx[i].addEventListener("input", OnInput, false);
-}
-
-function OnInput() {
-    this.style.height = "auto";
-    this.style.height = (this.scrollHeight) + "px";
-}
