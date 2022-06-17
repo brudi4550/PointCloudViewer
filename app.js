615cb5d2bf11dcf8c8c381513da6ce4d1c9ed076
paul2803-byte <73904569+paul2803-byte@users.noreply.github.com>
2022-06-17 12:14:09 +0200

    Await upload before convert

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');               // Used for manipulation with path
 const fs = require('fs-extra');             // Classic fs
 const port = 3000;
 const { exec, execFile } = require("child_process");
+const { stderr } = require('process');
+const path_to_las = 'C:/Users/Asus/OneDrive/Desktop/Uni/SS2022/IT_Projekt/PointCloudViewer/';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -54,25 +56,59 @@ app.get('/upload', (req, res) => {
   })
 })
 
-
-app.route('/upload').post((req, res, next) => {
+app.route('/upload').post(async (req, res, next) => {
     console.log('received post request');
-    req.pipe(req.busboy); // Pipe it trough busboy
-    req.busboy.on('file', (fieldname, file, filename) => {
-        console.log(`Upload of '${filename}' started`);
-        // Create a write stream of the new file
-        //fix naming + file type of uploaded file
-        const fstream = fs.createWriteStream(path.join(uploadPath, fieldname));
-        // Pipe it trough
-        file.pipe(fstream);
-        // On finish of the upload
-        fstream.on('close', () => {
-            console.log(`Upload of '${filename}' finished`);
-            res.redirect('back');
+    var awaitUpload = new Promise(function(resolve, reject) {
+      try{
+        req.pipe(req.busboy); // Pipe it trough busboy
+        req.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+          console.log(fieldname)
+          console.log(filename.filename)
+          console.log(`Upload of '${filename.filename}' started`);
+          // Create a write stream of the new file
+          //fix naming + file type of uploaded file
+          const fstream = fs.createWriteStream(path.join(uploadPath, filename.filename));
+          // Pipe it trough
+          file.pipe(fstream);
+          // On finish of the upload
+          fstream.on('close', () => {
+              console.log(`Upload of '${filename.filename}' finished`);
+              resolve(filename.filename)
+          });
         });
+      }catch(error){
+        reject(error)
+      }
+    })
+    awaitUpload.then(resolve => {
+      convertFilePromise(resolve).then(resolve2  => {
+        console.log('Request has been returned')
+        res.redirect('back')
+      })
     });
 });
 
 app.listen(port, () => {
   console.log(`PointCloudViewer listening on port ${port}`)
 })
+
+function convertFilePromise(filename){
+  console.log(`Converting the file ${filename} has started`);
+  const path_local = 'C:/Users/Asus/OneDrive/Desktop/Uni/SS2022/IT_Projekt/PointCloudViewer/';
+  return new Promise(function(resolve, reject) {
+    exec(`${path_local}PotreeConverter/PotreeConverter.exe ${path_local}fu/${filename} -o ${path_local}/fu/test -generate-page testpage`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        reject(error)
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        reject(stderr)
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      resolve('success')
+    });
+  });
+}
